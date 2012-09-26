4a8ac2e290b383a90168ab5f1e7e2d82613809c0
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-26 11:27:19 -0300

    Initial Commit

---
+++ AppUniversal-InternacionalizacaoTests/AppUniversal_InternacionalizacaoTests.h
@@ -0,0 +1,13 @@
+//
+//  AppUniversal_InternacionalizacaoTests.h
+//  AppUniversal-InternacionalizacaoTests
+//
+//  Created by Rafael Brigagão Paulino on 26/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AppUniversal_InternacionalizacaoTests : SenTestCase
+
+@end
